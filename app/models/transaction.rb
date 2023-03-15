c1373451c7dff7207b5e37752639833b330cb8dd
Dave Ng <wcng77@hotmail.com>
2023-03-16 00:36:43 +0800

    added comments for enum in transaction.rb

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,17 @@ class Transaction < ApplicationRecord
   belongs_to :income
   belongs_to :expense
   belongs_to :user
+
+  enum type: {
+    income: 'income',
+    transfer: 'transfer',
+    expense: 'expense'
+  }
+  # After defining the enum, you can use it in your application code like this:
+  # create a new transaction with type 'income'
+  # t = Transaction.new(type: :income, description: 'Salary', amount: 1000.0, date: Date.today)
+
+  # When you save the transaction to the database,
+  # the type attribute will be stored as a string value corresponding to the named value you specified in the enum.
+  # For example, if you set type to :income, the value will be stored as the string 'income' in the database.
 end
