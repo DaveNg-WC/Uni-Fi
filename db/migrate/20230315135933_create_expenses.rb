63084672152ec1940585956b9e650b64d8186aef
Dave Ng <wcng77@hotmail.com>
2023-03-15 22:11:28 +0800

    removed description for Income and Expense table from migration file

--- db/migrate/20230315135933_create_expenses.rb
+++ db/migrate/20230315135933_create_expenses.rb
@@ -2,7 +2,6 @@ class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
       t.string :name
-      t.text :description
       t.float :balance
       t.references :user, null: false, foreign_key: true
 
--- db/migrate/20230315140540_create_incomes.rb
+++ db/migrate/20230315140540_create_incomes.rb
@@ -2,7 +2,6 @@ class CreateIncomes < ActiveRecord::Migration[7.0]
   def change
     create_table :incomes do |t|
       t.string :name
-      t.text :description
       t.float :balance
       t.references :user, null: false, foreign_key: true
 
