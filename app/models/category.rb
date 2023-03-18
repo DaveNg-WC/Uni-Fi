f785a23dcb806c622a762d4b2ed52f038e4f0ec9
Dave Ng <wcng77@hotmail.com>
2023-03-18 14:40:23 +0800

    set enum type:income,expense for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   belongs_to :user
+
+  enum type: {
+    income: 'income',
+    expense: 'expense'
+  }
+  # After defining the enum, you can use it in your application code like this:
+  # create a new category with type 'income'
+  # t = Category.new(type: :income, name: "food", user_id: 1)
 end
