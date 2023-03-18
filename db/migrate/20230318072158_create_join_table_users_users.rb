db4f1f2ac59faed15f57703ae1fe0aabfc5d9716
Dave Ng <wcng77@hotmail.com>
2023-03-18 15:36:40 +0800

    created jointable for partners_users

---
+++ db/migrate/20230318072158_create_join_table_users_users.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableUsersUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :users, :partners do |t|
+      t.index :user_id
+      t.index :partner_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_18_071007) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_18_072158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_18_071007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "partners_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "partner_id", null: false
+    t.index ["partner_id"], name: "index_partners_users_on_partner_id"
+    t.index ["user_id"], name: "index_partners_users_on_user_id"
   end
 
   create_table "transactions", force: :cascade do |t|
