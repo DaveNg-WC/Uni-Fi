e4bb9e6866c46cede3053f9f28dbade86f038225
Dave Ng <wcng77@hotmail.com>
2023-03-18 14:35:31 +0800

    set enum type:credit,debit for wallet model

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,11 @@
 class Wallet < ApplicationRecord
   belongs_to :user
+
+  enum type: {
+    debit: 'debit',
+    credit: 'credit'
+  }
+  # After defining the enum, you can use it in your application code like this:
+  # create a new transaction with type 'income'
+  # t = Transaction.new(type: :income, description: 'Salary', amount: 1000.0, date: Date.today)
 end
