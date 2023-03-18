4303a664414dcdc297ae15ab0b96674e72d25d2e
Dave Ng <wcng77@hotmail.com>
2023-03-18 15:39:12 +0800

    edited name of user_partner migration

--- db/migrate/20230318072158_create_join_table_users_partners.rb
+++ db/migrate/20230318072158_create_join_table_users_partners.rb
@@ -1,4 +1,4 @@
-class CreateJoinTableUsersUsers < ActiveRecord::Migration[7.0]
+class CreateJoinTableUsersPartners < ActiveRecord::Migration[7.0]
   def change
     create_join_table :users, :partners do |t|
       t.index :user_id
