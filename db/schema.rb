92869c6502ec51e570e96388cedca6f64fb58767
Dave Ng <wcng77@hotmail.com>
2023-03-18 14:37:24 +0800

    scaffolded category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_18_063049) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_18_063623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,5 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_18_063049) do
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "wallets", "users"
 end
