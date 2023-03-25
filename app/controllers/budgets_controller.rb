d11f5192dae714bda3f889eb5654fbd0e2f23cc5
Yi Suan Lee <l.yixuane@gmail.com>
2023-03-25 12:26:40 +0800

    Updated budget form

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,17 +1,15 @@
 class BudgetsController < ApplicationController
   before_action :set_budget, only: %i[ show edit update destroy ]
 
-  # GET /budgets or /budgets.json
+  # GET /budgets
   def index
-    @budgets = Budget.all
+    @budgets = Budget.all.order(:name)
   end
 
-  # GET /budgets/1 or /budgets/1.json
+  # GET /budgets/1
   def show
     @budget = Budget.find(params[:id])
     @category = Category.find(1)
-    # @target_budget = @category.budgets.sum(:amount)
-    # @current_spending = @category.expenses.sum(:amount)
   end
 
   def new
@@ -25,11 +23,8 @@ class BudgetsController < ApplicationController
     @categories = current_user.categories.where(category_type: 'expense')
   end
 
-  # POST /budgets or /budgets.json
+  # POST /budgets
   def create
-    category_ids = params[:budget].delete(:category_ids) || []
-    params[:budget][:categories] = Category.where(id: category_ids)
-
     @budget = Budget.new(budget_params)
 
     if @budget.save
@@ -39,7 +34,7 @@ class BudgetsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /budgets/1 or /budgets/1.json
+  # PATCH/PUT
   def update
     respond_to do |format|
       if @budget.update(budget_params)
@@ -52,7 +47,7 @@ class BudgetsController < ApplicationController
     end
   end
 
-  # DELETE /budgets/1 or /budgets/1.json
+  # DELETE /budgets/1
   def destroy
     @budget.destroy
 
@@ -70,36 +65,8 @@ class BudgetsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def budget_params
-    params.require(:budget).permit(:amount, :name, :target_budget, categories: [])
+    # category_ids = params[:budget][:category_ids]&.reject(&:blank?)
+    # category_objs = Category.where(id: category_ids)
+    params.require(:budget).permit(:amount, :name, category_ids: [])
   end
 end
-
-# POST /budgets or /budgets.json
-# def create
-#   # @budget = Budget.new(budget_params)
-#   # @user_id = current_user
-#   # @budget.user_id = @user_id
-#   # @budget.save
-
-#   # respond_to do |format|
-#   #   if @budget.save
-#   #     format.html { redirect_to budget_url(@budget), notice: "Budget was successfully created." }
-#   #     format.json { render :show, status: :created, location: @budget }
-#   #   else
-#   #     format.html { render :new, status: :unprocessable_entity }
-#   #     format.json { render json: @budget.errors, status: :unprocessable_entity }
-#   #   end
-
-
-#   #   @budget = Budget.new(budget_params)
-#   #   @budget.categories = Category.where(id: params[:budget][:category_ids])
-
-#   #   if @budget.save
-#   #     redirect_to @budget
-#   #   else
-#   #     render :new
-#   #   end
-#   # end
-
-# # GET /budgets/new
-# end
