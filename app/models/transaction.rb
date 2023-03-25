f130620a0d1f8a7df6ee03d98f6e5483265f7868
Dave Ng <wcng77@hotmail.com>
2023-03-25 15:05:27 +0800

    added transactions list for categories

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,5 @@ class Transaction < ApplicationRecord
   belongs_to :main_wallet, class_name: 'Wallet', foreign_key: 'main_wallet_id'
   belongs_to :second_wallet, class_name: 'Wallet', foreign_key: 'second_wallet_id', optional: true
   validates :main_wallet, presence: true
+
 end
