71b0efa855026305ce2bc0ef155d339009d0546f
Ezekiel Lee <dollarmiles100@gmail.com>
2023-04-05 02:31:14 +0800

    working bank statement upload / toggle included in dashboard / edited navbar1 / minor fix in budgets show and index

--- app/controllers/statement_controller.rb
+++ app/controllers/statement_controller.rb
@@ -2,8 +2,11 @@ require "pp"
 require 'open-uri'
 require 'pdf-reader'
 require 'date'
+require 'regex_mapping_helper'
 
 class StatementController < ApplicationController
+  after_action :delete_uploaded_txns, only: [:uploaded]
+
   UPLOADED_TXNS = []
   def new
   end
@@ -21,37 +24,31 @@ class StatementController < ApplicationController
       text = page.text
       array = text.split("\n")
 
-      # example = " 10 Feb           BUS/MRT 228880949     SINGAPORE     SG 05 FEB                $                2.76"
       regex3 = /\s+(\d{2}\s{1}\D{3})\s+(.+)\s{1}(\d{2}\s{1}\D{3})(\s+\W\s+)(\d{1,}.\d{2})/
-      # if example.match(regex3)
-      #   match = example.match(regex3)
-      #   newTxn = Transaction.new(description: match[2].to_s,
-      #                         txn_type: "Expense",
-      #                         user_id: current_user.id,
-      #                         amount: match[5],
-      #                         category_id: 3,
-      #                         date: Date.parse("#{match[3]} #{Date.today.year}"),
-      #                         main_wallet_id: 1)
-      #                         # main wallet here should be taken from above
-      #   newTxn.save
-      #   UPLOADED_TXNS << newTxn
-      # else
-      # end
 
       array.each do |line|
         if line.match(regex3)
           match = line.match(regex3)
-          newTxn = Transaction.new(description: match[2].to_s,
-                      txn_type: "Expense",
-                      user_id: current_user.id,
-                      amount: match[5],
-                      category_id: 3,
-                      date: Date.parse("#{match[3]} #{Date.today.year}"),
-                      main_wallet_id: 1)
-                      # main wallet here should be taken from above
-          newTxn.save
-          UPLOADED_TXNS << newTxn
+          RegexMappingHelper::REGEX_MAPPING.each do |regex, category_id|
+            if match[2].match(regex)
+              break
+              # correct as of 4 april 5:42pm
+              # category_id = "transport"
+            else
+            end
+
+            newTxn = Transaction.new(description: match[2].to_s,
+                          txn_type: "Expense",
+                          user_id: current_user.id,
+                          amount: match[5],
+                          category_id: 3,
+                          date: Date.parse("#{match[3]} #{Date.today.year}"),
+                          main_wallet_id: 1)  # main wallet here should be taken from above field, user choose wallet
+            newTxn.save
+            UPLOADED_TXNS << newTxn
+          end
         else
+
         end
       end
     end
@@ -61,5 +58,11 @@ class StatementController < ApplicationController
 
   def uploaded
     @array = UPLOADED_TXNS
+  end
+
+  private
+
+  def delete_uploaded_txns
+    UPLOADED_TXNS.clear
   end
 end
