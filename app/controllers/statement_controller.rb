8d84d2dbd32df1baf5596694a79c69ef47689d6d
Ezekiel Lee <dollarmiles100@gmail.com>
2023-04-06 15:08:38 +0800

    completed regexmapping, statement controller

--- app/controllers/statement_controller.rb
+++ app/controllers/statement_controller.rb
@@ -9,6 +9,7 @@ class StatementController < ApplicationController
 
   UPLOADED_TXNS = []
   def new
+    @wallets = Wallet.all
   end
 
   def create
@@ -23,7 +24,6 @@ class StatementController < ApplicationController
     reader.pages.each do |page|
       text = page.text
       array = text.split("\n")
-
       regex3 = /\s+(\d{2}\s{1}\D{3})\s+(.+)\s{1}(\d{2}\s{1}\D{3})(\s+\W\s+)(\d{1,}.\d{2})/
 
       array.each do |line|
@@ -31,24 +31,29 @@ class StatementController < ApplicationController
           match = line.match(regex3)
           RegexMappingHelper::REGEX_MAPPING.each do |regex, category_id|
             if match[2].match(regex)
+              newTxn = Transaction.new(description: match[2].to_s,
+                                       txn_type: "Expense",
+                                       user_id: current_user.id,
+                                       amount: match[5],
+                                       category_id: category_id,
+                                       date: Date.parse("#{match[3]} #{Date.today.year}"),
+                                       main_wallet_id: 1)  # main wallet here should be taken from above field, user choose wallet
+              newTxn.save
+              UPLOADED_TXNS << newTxn
               break
-              # correct as of 4 april 5:42pm
               # category_id = "transport"
-            else
             end
-
-            newTxn = Transaction.new(description: match[2].to_s,
-                          txn_type: "Expense",
-                          user_id: current_user.id,
-                          amount: match[5],
-                          category_id: 3,
-                          date: Date.parse("#{match[3]} #{Date.today.year}"),
-                          main_wallet_id: 1)  # main wallet here should be taken from above field, user choose wallet
-            newTxn.save
-            UPLOADED_TXNS << newTxn
+            # newTxn = Transaction.new(description: match[2].to_s,
+            #               txn_type: "Expense",
+            #               user_id: current_user.id,
+            #               amount: match[5],
+            #               category_id: 9, # category id
+            #               date: Date.parse("#{match[3]} #{Date.today.year}"),
+            #               main_wallet_id: 1)  # main wallet here should be taken from above field, user choose wallet
+            # newTxn.save
+            # UPLOADED_TXNS << newTxn
+            # break
           end
-        else
-
         end
       end
     end
