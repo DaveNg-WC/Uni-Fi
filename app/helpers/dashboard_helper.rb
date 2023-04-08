46116a0896eea1b4e27f9eb5ab0459acf00a59b8
Dave Ng <wcng77@hotmail.com>
2023-04-08 17:56:08 +0800

    done combined income vs expense year

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -87,6 +87,22 @@ module DashboardHelper
     month_spends_breakdown    = month_spends_breakdown.sort_by { |k, v| -v }.to_h
   end
 
+  def year_spends_breakdown(user)
+    ## Returns a hash. {cat.name: cat balance} sorted Descending
+    year_spends_breakdown = {}
+    user.categories.each do |c|
+      # skip if category is income
+      next if c.income?
+
+      balance = 0
+      c.transactions.where(date: Date.current.all_year).each do |t|
+        balance += t.amount
+      end
+      year_spends_breakdown.store(c.name, balance)
+    end
+    year_spends_breakdown    = year_spends_breakdown.sort_by { |k, v| -v }.to_h
+  end
+
   def combined_week_spends_breakdown
     week_spends_breakdown(current_user).merge(week_spends_breakdown(current_user.partner)) do |key, old_value, new_value|
       old_value + new_value
@@ -95,6 +111,12 @@ module DashboardHelper
 
   def combined_month_spends_breakdown
     month_spends_breakdown(current_user).merge(month_spends_breakdown(current_user.partner)) do |key, old_value, new_value|
+      old_value + new_value
+    end
+  end
+
+  def combined_year_spends_breakdown
+    year_spends_breakdown(current_user).merge(year_spends_breakdown(current_user.partner)) do |key, old_value, new_value|
       old_value + new_value
     end
   end
@@ -131,6 +153,24 @@ module DashboardHelper
     # returns an combined hash
     user        = total_expense(current_user).where(date: Date.current.all_month).group_by_month(:date, format: "%b").sum(:amount)
     partner     = total_expense(current_user.partner).where(date: Date.current.all_month).group_by_month(:date, format: "%b").sum(:amount)
+    merged_hash = user.merge(partner) do |key, old_value, new_value|
+      old_value + new_value
+    end
+  end
+
+  def combined_income_this_year
+    # returns an combined hash
+    user        = total_income(current_user).where(date: Date.current.all_year).group_by_month(:date, format: "%b").sum(:amount)
+    partner     = total_income(current_user.partner).where(date: Date.current.all_year).group_by_month(:date, format: "%b").sum(:amount)
+    merged_hash = user.merge(partner) do |key, old_value, new_value|
+      old_value + new_value
+    end
+  end
+
+  def combined_expense_this_year
+    # returns an combined hash
+    user        = total_expense(current_user).where(date: Date.current.all_year).group_by_month(:date, format: "%b").sum(:amount)
+    partner     = total_expense(current_user.partner).where(date: Date.current.all_year).group_by_month(:date, format: "%b").sum(:amount)
     merged_hash = user.merge(partner) do |key, old_value, new_value|
       old_value + new_value
     end
